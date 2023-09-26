84daec43653dae17ee85893390e258d0888ea069
Caique <caiquealvesdesouza27@gmail.com>
2023-09-26 14:46:09 -0300

    Transferência da responsabilidade de criação de conta para o backend

--- Web/src/axios/api.js
+++ Web/src/axios/api.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:8877",
+	baseURL: "http://localhost:8877",
 });
 
 export const busca = async (url) => {
-  const resposta = await api.get(url);
-  return resposta.data;
+	const resposta = await api.get(url);
+	return resposta.data;
 };
 export const adicionar = async (url, novaConta) => {
-  await api.post(url, novaConta);
+	const resposta = await api.post(url, novaConta);
+	return resposta.data;
 };
-export const atualizar = async (url,alteracoesConta)=>{
-  await api.put(url,alteracoesConta)
+export const atualizar = async (url, alteracoesConta) => {
+	await api.put(url, alteracoesConta);
 };
-export const excluir = async(url)=>{
-  await api.delete(url)
-}
+export const excluir = async (url) => {
+	await api.delete(url);
+};
--- Web/src/pages/CriarConta/CriarConta.jsx
+++ Web/src/pages/CriarConta/CriarConta.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./CriarConta.css";
 import Image from "../../assets/img/Logo.png";
-import { Typography, Box, Tabs, Tab,Alert,Snackbar } from "@mui/material";
+import { Typography, Box, Tabs, Tab, Alert, Snackbar } from "@mui/material";
 import Stepper from "../../components/Stepper/Stepper.jsx";
 import SwipeableViews from "react-swipeable-views";
 import DadosPessoais from "../../components/Cadastro/DadosPessoais.jsx";
@@ -11,144 +11,135 @@ import Feedback from "../../components/Cadastro/Feedback.jsx";
 import { useTheme } from "@mui/material/styles";
 import { adicionar, busca } from "../../axios/api.js";
 function CriarConta() {
-  const theme = useTheme();
-  const [etapaAtual, setEtapaAtual] = useState(0);
-  const [dadosColetados, setDados] = useState({});
-  const [buttonStatus,setButtonStatus] = useState("waiting")
-  const [numeroFeedback, setNumeroFeedback] = useState();
+	const theme = useTheme();
+	const [etapaAtual, setEtapaAtual] = useState(0);
+	const [dadosColetados, setDados] = useState({});
+	const [buttonStatus, setButtonStatus] = useState("waiting");
+	const [numeroFeedback, setNumeroFeedback] = useState();
 
-  const [open,setOpen]= useState(false)
-  const handleClose = () => {
-    setOpen(false);
-  };
+	const [open, setOpen] = useState(false);
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-  const componenteConfirmacao = (
-    <ConfirmarDados
-      voltarPagina={anterior}
-      registrarConta={registrarConta}
-      status={buttonStatus}
-      dados={dadosColetados}
-      key={2}
-    />
-  );
-  const pages = [
-    <DadosPessoais proximaPagina={proximo} atualizar={atualizar} key={0} />,
-    <DadosLogin
-      voltarPagina={anterior}
-      proximaPagina={proximo}
-      atualizar={atualizar}
-      key={1}
-    />,
-    etapaAtual == 2 ? componenteConfirmacao : <h1 key={2}>confirmacao</h1>,
-    <Feedback numeroConta={numeroFeedback} key={3} />,
-  ];
-  function proximo() {
-    if (etapaAtual < 3) {
-      setEtapaAtual(etapaAtual + 1);
-      setNumeroConta();
-    }
-  }
-  function anterior() {
-    if (etapaAtual > 0) {
-      setEtapaAtual(etapaAtual - 1);
-    }
-  }
-  function atualizar(dados) {
-    //console.log(data[0]);
+	const componenteConfirmacao = (
+		<ConfirmarDados
+			voltarPagina={anterior}
+			registrarConta={registrarConta}
+			status={buttonStatus}
+			dados={dadosColetados}
+			key={2}
+		/>
+	);
+	const pages = [
+		<DadosPessoais proximaPagina={proximo} atualizar={atualizar} key={0} />,
+		<DadosLogin
+			voltarPagina={anterior}
+			proximaPagina={proximo}
+			atualizar={atualizar}
+			key={1}
+		/>,
+		etapaAtual == 2 ? componenteConfirmacao : <h1 key={2}>confirmacao</h1>,
+		<Feedback numeroConta={numeroFeedback} key={3} />,
+	];
+	function proximo() {
+		if (etapaAtual < 3) {
+			setEtapaAtual(etapaAtual + 1);
+		}
+	}
+	function anterior() {
+		if (etapaAtual > 0) {
+			setEtapaAtual(etapaAtual - 1);
+		}
+	}
+	function atualizar(dados) {
+		//console.log(data[0]);
 
-    if (etapaAtual == 0) {
-      setDados({ ...dadosColetados, ...dados });
-    } else if (etapaAtual == 1) {
-      setDados({ ...dadosColetados, ...dados });
-    }
-  }
-  async function setNumeroConta() {
-    
-        var contas = await busca("/contas");
-        var numeros = [];
-        for (var conta in contas) {
-          numeros.push(contas[conta]._numeroConta);
-        }
+		if (etapaAtual == 0) {
+			setDados({ ...dadosColetados, ...dados });
+		} else if (etapaAtual == 1) {
+			setDados({ ...dadosColetados, ...dados });
+		}
+	}
 
-        var num = Math.floor(Math.random() * 9999) + 1;
+	async function formatarConta() {
+		return {
+			_nomeCliente: dadosColetados.nome,
+			genero: dadosColetados.genero,
+			_senha: dadosColetados.senha,
+			_saldo: 100,
+			agencia: "0001",
+			_transacoes: [],
+		};
+	}
 
-        if (numeros.includes(num) == false) {
-          return num;
-        }
-    
-    
-  }
+	async function registrarConta() {
+		//   await adicionar('/contas',novaConta)
+		setButtonStatus("loading");
+		try {
+			const contaFormatada = await formatarConta();
+			var numeroConta = (await adicionar("/contas", contaFormatada)).info;
+			setNumeroFeedback(numeroConta);
 
-  async function formatarConta() {
-    return {
-      _numeroConta: await setNumeroConta(),
-      _nomeCliente: dadosColetados.nome,
-      genero: dadosColetados.genero,
-      _senha: dadosColetados.senha,
-      _saldo: 100,
-      agencia: "0001",
-      _transacoes: [],
-    };
-  }
+			proximo();
+		} catch {
+			console.log("erro");
+			setOpen(true);
+			setTimeout(() => {
+				setButtonStatus("waiting");
+			}, 2500);
+		}
+	}
+	return (
+		<section className="CriarConta-page">
+			<header className="CriarConta-header">
+				<img src={Image} alt="Logo" className="CriarConta-logo" />
+				<h1 className="CriarConta-nome">CaiqueBank</h1>
+			</header>
 
-  async function registrarConta() {
-    //   await adicionar('/contas',novaConta)
-    setButtonStatus("loading")
-    try{
-      const contaFormatada = await formatarConta();
-      setNumeroFeedback(contaFormatada._numeroConta);
-      proximo();
-
-      await adicionar("/contas", contaFormatada);
-    }catch{
-      console.log('erro')
-      setOpen(true)
-      setTimeout(() => {
-        setButtonStatus("waiting")
-      }, 2500);
-    }
-    
-  }
-  return (
-    <section className="CriarConta-page">
-      <header className="CriarConta-header">
-        <img src={Image} alt="Logo" className="CriarConta-logo" />
-        <h1 className="CriarConta-nome">CaiqueBank</h1>
-      </header>
-
-      <div className="CriarConta-box">
-        <Box>
-          <Tabs
-            sx={{ borderBottom: 0.5, borderColor: "divider" }}
-            value={0}
-            variant="fullWidth"
-            textColor="secondary"
-            indicatorColor="primary"
-          >
-            <Tab
-              label="Criar Conta"
-              disableRipple
-              sx={{ fontFamily: "proxima nova", fontSize: "110%" }}
-            />
-          </Tabs>
-        </Box>
-        <Stepper currentStep={etapaAtual} />
-        <SwipeableViews
-          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-          index={etapaAtual}
-        >
-          {pages}
-        </SwipeableViews>
-        <Snackbar open={open} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{vertical: 'bottom',
-          horizontal: 'center', }}
->
-        <Alert variant="filled" severity="error" sx={{ width: '100%' }}>
-          Falha ao criar conta. Tente novamente mais tarde.
-        </Alert>
-      </Snackbar>
-      </div>
-    </section>
-  );
+			<div className="CriarConta-box">
+				<Box>
+					<Tabs
+						sx={{ borderBottom: 0.5, borderColor: "divider" }}
+						value={0}
+						variant="fullWidth"
+						textColor="secondary"
+						indicatorColor="primary"
+					>
+						<Tab
+							label="Criar Conta"
+							disableRipple
+							sx={{
+								fontFamily: "proxima nova",
+								fontSize: "110%",
+							}}
+						/>
+					</Tabs>
+				</Box>
+				<Stepper currentStep={etapaAtual} />
+				<SwipeableViews
+					axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+					index={etapaAtual}
+				>
+					{pages}
+				</SwipeableViews>
+				<Snackbar
+					open={open}
+					autoHideDuration={3000}
+					onClose={handleClose}
+					anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+				>
+					<Alert
+						variant="filled"
+						severity="error"
+						sx={{ width: "100%" }}
+					>
+						Falha ao criar conta. Tente novamente mais tarde.
+					</Alert>
+				</Snackbar>
+			</div>
+		</section>
+	);
 }
 
 export default CriarConta;
